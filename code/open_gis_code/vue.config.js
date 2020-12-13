3870b82936c9451b9ae348ef2122f0e3cf267fbf
yuanzhaokang <kangzhaoyuan@gmail.com>
2020-12-13 11:14:14 +0800

    fix: vue config

--- code/open_gis_code/vue.config.js
+++ code/open_gis_code/vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
   devServer: {
     port: 8081,
     proxy: {
-      '/geoserver': {
-        target: 'http://localhost:8080'
-      },
+      // '/geoserver': {
+      //   target: 'http://localhost:8080'
+      // },
       '/cdn': {
         target: 'http://192.168.192.128'
       }
