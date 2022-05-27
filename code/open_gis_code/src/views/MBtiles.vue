5a8903b37629737b32a10318c1eea2968734dc50
yhdjyyzk <2503144553@qq.com>
2022-05-27 18:10:16 +0800

    feat: mapbox 主题

--- code/open_gis_code/src/views/MBtiles.vue
+++ code/open_gis_code/src/views/MBtiles.vue
@@ -1,31 +1,113 @@
 <template>
-  <div id="app">
+  <div class="map-container">
+    <div class="panel">
+      <el-row>
+        <el-col :span="8">当前层级</el-col>
+        <el-col :span="16">{{ zoom }}</el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="8">地图样式</el-col>
+        <el-col :span="16">
+          <el-select v-model="style" size="mini">
+            <el-option
+              v-for="item in themes"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-col>
+      </el-row>
+    </div>
     <div ref="map" class="map"></div>
   </div>
 </template>
 
 <script>
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {},
-  mounted () {
-    const map = new mapboxgl.Map({
+  data() {
+    return {
+      zoom: 3,
+      themes: [
+        {
+          label: "street-style",
+          value: "/static/map/style/street-style.json",
+        },
+        {
+          label: "basic-style",
+          value: "/static/map/style/basic-style.json",
+        },
+        {
+          label: "mapbox-street-style",
+          value: "/static/map/style/mapbox-street-style.json",
+        },
+        {
+          label: "fiord-color-gl-style",
+          value:
+            "/static/map/style/fiord-color-gl-style/fiord-color-gl-style.json",
+        },
+        {
+          label: "night-style",
+          value: "/static/map/style/night-style.json",
+        }
+      ],
+      style: "/static/map/style/fiord-color-gl-style/fiord-color-gl-style.json",
+    };
+  },
+  watch: {
+    style() {
+      this.$map.setStyle(`http://localhost:3000${this.style}`);
+    },
+  },
+  mounted() {
+    this.$map = new mapboxgl.Map({
       container: this.$refs.map,
       // style: '/static/map/style/style.json',
-      style: 'http://localhost:3000/static/map/style/night-style.json',
+      style:
+        "http://localhost:3000/static/map/style/fiord-color-gl-style/fiord-color-gl-style.json",
       center: [116.391229827904, 39.907092084593216], // starting position [lng, lat]
-      zoom: 10, // starting zoom
-      antialias: true
-    })
-  }
-}
+      zoom: this.zoom, // starting zoom
+      antialias: true,
+    });
+
+    this.$map.on("zoomend", (e) => {
+      this.zoom = this.$map.getZoom().toFixed(1);
+    });
+
+    this.$map.on("click", (e) => {
+      const feature = this.$map.queryRenderedFeatures(e.point);
+
+      if (feature.length) {
+        console.log(feature[0].layer);
+      }
+    });
+  },
+};
 </script>
 
-<style>
-.map {
+<style lang="less">
+.map-container {
   height: 100%;
+
+  .map {
+    height: 100%;
+  }
+
+  .panel {
+    width: 300px;
+    position: absolute;
+    z-index: 100000;
+    top: 0px;
+    right: 0px;
+    padding: 8px;
+    background-color: rgb(58, 67, 91);
+  }
 }
 </style>
